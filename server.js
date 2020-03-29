a5dc7e5681e938b4d0c1d0fa37624e63c8264731
srikant vs <srikantvs26@gmail.com>
2020-03-29 12:57:06 +0530

    update to deploy to heroku 6

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(allowCrossDomain);
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/OnlineDietSystem'));
