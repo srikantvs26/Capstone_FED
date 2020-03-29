ba98de4c20aae3c1f6637f912930f97ecd6e1e06
srikant vs <srikantvs26@gmail.com>
2020-03-29 13:03:54 +0530

    update to deploy to heroku 7

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(allowCrossDomain);
+
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/OnlineDietSystem'));
 
 app.get('/*', function(req,res) {
-    
+res.header('Access-Control-Allow-Origin', '*');
 res.sendFile(path.join(__dirname,'/dist/OnlineDietSystem/index.html'));
 });
 
