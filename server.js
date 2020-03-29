ba3a10ac8a477176b073850e6b51857320452740
srikant vs <srikantvs26@gmail.com>
2020-03-29 12:14:20 +0530

    update to deploy to heroku 4

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express=require('express');
+//Install express server
+const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + 'dist/OnlineDietSystem'));
-app.get('/*', function (req, res) { 
-    res.sendFile(path.join(__dirname + 'dist/OnlineDietSystem/index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/OnlineDietSystem'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/OnlineDietSystem/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
