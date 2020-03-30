f014aecee28d7781064f6740d92f2b9d13c1c52a
srikant vs <srikantvs26@gmail.com>
2020-03-30 11:27:16 +0530

    removed all localhost

--- src/app/admin/search-by-email.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchByEmailPipe } from './search-by-email.pipe';
-
-describe('SearchByEmailPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchByEmailPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/user/search-by-email.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchByMailPipe } from './search-by-email.pipe';
-
-describe('SearchByMailPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchByMailPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
